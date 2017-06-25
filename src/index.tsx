ec41140ee4c2c4bae117afdc214d5c5717af772a
miguelvazq <miguelvazq@users.noreply.github.com>
2017-06-24 21:14:43 -0400

    Added css loader and assets /css

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Hello } from "./components/Hello";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './container/App';
+import './assets/styles.css';
+//import { Hello } from './components/Hello';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <App title='Configuration Manager' framework='React' />,
+    document.getElementById('root')
 );
